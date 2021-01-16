b47d57289179b4bd005e278b95e37f7b99af62cc
FrapoDeveloper <fran_espino189@outlook.com>
2021-01-15 20:50:05 -0500

    web sgnkapp lista

--- src/components/Organisms/episodioscontainer.jsx
+++ src/components/Organisms/episodioscontainer.jsx
@@ -5,7 +5,7 @@ import withloader from '../Hoc/withloader.js'
 
 const Episodioscontainer = ({episodios}) => (   
     <>
-    <h1 className="titulo-episodios">Episodios Sgnk The final season </h1>
+    <h1 className="titulo-episodios">Episodios de Sgnk The final season</h1>
     <div  className="ed-grid  grid-container s-mt-1 m-grid-2 s-grid-1 xl-grid-3 lg-grid-3 center">
 
         { episodios.map(e => (
